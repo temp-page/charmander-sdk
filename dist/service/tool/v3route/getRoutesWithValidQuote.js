51e76d5e06c189807be2ed45129f4501776b0ddb
lihang941 <1329075357@qq.com>
2023-11-24 17:38:45 +0000

    feat: cjs

---
+++ dist/service/tool/v3route/getRoutesWithValidQuote.js
@@ -0,0 +1,22 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.getRoutesWithValidQuote = void 0;
+const sdk_1 = require("../sdk");
+const functions_1 = require("./functions");
+async function getRoutesWithValidQuote({ amount, baseRoutes, distributionPercent, quoteProvider, tradeType, gasModel, }) {
+    const [percents, amounts] = (0, functions_1.getAmountDistribution)(amount, distributionPercent);
+    // 拼route
+    const routesWithoutQuote = amounts.reduce((acc, curAmount, i) => [
+        ...acc,
+        ...baseRoutes.map(r => ({
+            ...r,
+            amount: curAmount,
+            percent: percents[i],
+        })),
+    ], []);
+    const getRoutesWithQuote = tradeType === sdk_1.TradeType.EXACT_INPUT
+        ? quoteProvider.getRouteWithQuotesExactIn
+        : quoteProvider.getRouteWithQuotesExactOut;
+    return getRoutesWithQuote(routesWithoutQuote, { gasModel });
+}
+exports.getRoutesWithValidQuote = getRoutesWithValidQuote;
