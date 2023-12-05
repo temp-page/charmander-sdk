016a8eb900c5b37ab2b323e9a3d0f4f6e36f01cf
lihang941 <1329075357@qq.com>
2023-12-05 10:16:03 +0000

    feat: 显示质押币种

---
+++ dist/mulcall/types.d.ts
@@ -0,0 +1,10 @@
+import type { ParamType } from 'ethers';
+export interface ContractCall {
+    contract: {
+        address: string;
+    };
+    name: string;
+    inputs: ParamType[];
+    outputs: ParamType[];
+    params: any[];
+}
